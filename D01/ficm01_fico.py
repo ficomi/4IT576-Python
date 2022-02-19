533372fb0736421ddc7d0c92d9360e36c003a05b
Miloslav Fico <ficomiloslav@seznam.cz>
2022-02-19 02:24:34 +0100

    du1

---
+++ D01/ficm01_fico.py
@@ -0,0 +1,15 @@
+#Hlavní zdroj informací byl vaše internetové stránky.
+#Nepotřebuji nic dovysvětlovat.
+
+
+def author_name():
+	return "FICM01"
+
+def os():
+	return "Windows 10, Pop!_OS"
+
+def languages():
+	return "Java - >2000\nC# - <2000 \nSmalltalk - <2000 (využívané v práci)\nApex - <2000 \nPHP <2000"
+
+def my_history():
+	return "První zkušenost ZŠ - Pascal. SŠ - Java, PHP. VŠ  - Java, C#. Práce - Smalltalk, Apex, Java"
